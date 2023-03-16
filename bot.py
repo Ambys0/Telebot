d00b840c82fdde71cf0d354c9a8e8a9e8060a1dd
Dmytro Rychkov <98288757+Ambys0@users.noreply.github.com>
2023-03-16 21:21:40 +0200

    Some small changes.

--- bot.py
+++ bot.py
@@ -1,39 +1,70 @@
+from datetime import datetime, timedelta
+import requests
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import requests
 
 
 TOKEN = '5478265131:AAGBHvDuwi-NMgxSkQxe-_aBGUQcfmLQrYA'
+API_KEY = '499612c952951b4ec1036569d9428a46'
 
-# створюємо бота та диспетчера
-bot = Bot("5478265131:AAGBHvDuwi-NMgxSkQxe-_aBGUQcfmLQrYA")
+bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
 
-# обробка команди /weather
-@dp.message_handler(commands=["weather"])
-async def get_weather_command(message: types.Message):
-    # виконуємо запит до API та повертаємо погодні дані
-    api_key = "499612c952951b4ec1036569d9428a46"
-    city = message.text.split("/weather ")[1]
-    weather = get_weather(city, api_key)
-
-    # відправляємо відповідь користувачу
-    await message.answer(weather)
-
-
-def get_weather(city: str, api_key: str) -> str:
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+def get_current_weather(city: str) -> str:
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}'
     response = requests.get(url)
     data = response.json()
-    if data["cod"] == "404":
-        return "Місто не знайдено"
-    else:
-        temp = data["main"]["temp"]
-        feels_like = data["main"]["feels_like"]
-        weather_description = data["weather"][0]["description"]
-        return f"Температура в місті {city}: {temp:.0f}°C, відчувається як {feels_like:.0f}°C. "
+
+    description = data['weather'][0]['description']
+    temperature = data['main']['temp']
+    feels_like = data['main']['feels_like']
+    humidity = data['main']['humidity']
+    wind_speed = data['wind']['speed']
+
+    current_weather = (f'Поточна погода в місті {city}: '
+                       f'Температура {temperature:.0f}°C, '
+                       f'відчувається як {feels_like:.0f}°C. '
+                       f'Вологість {humidity}%, вітер {wind_speed} м/с.')
+    return current_weather
+
+
+def get_tomorrow_weather(city: str) -> str:
+    url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    forecast_list = data['list']
+    tomorrow_date = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
+
+    for forecast in forecast_list:
+        forecast_date = forecast['dt_txt'].split(' ')[0]
+        if forecast_date == tomorrow_date:
+            description = forecast['weather'][0]
+            min_temp = forecast['main']['temp_min']
+            max_temp = forecast['main']['temp_max']
+            humidity = forecast['main']['humidity']
+            wind_speed = forecast['wind']['speed']
+
+            tomorrow_weather = (f'Прогноз на завтра: '
+                                f'Температура від {min_temp:.0f} до {max_temp:.0f}°C. '
+                                f'Вітер {wind_speed} м/с, вологість {humidity}%.')
+            return tomorrow_weather
+
+    return 'На завтра прогноз поки недоступний.'
+
+
+@dp.message_handler(commands=['weather'])
+async def handle_weather_command(message: types.Message):
+    try:
+        city = message.text.split('/weather ')[1]
+        current_weather = get_current_weather(city)
+        tomorrow_weather = get_tomorrow_weather(city)
+        response = f'{current_weather}\n\n{tomorrow_weather}'
+    except:
+        response = 'Не вдалося отримати погоду. Будь ласка, перевірте правильність назви міста та спробуйте ще раз.'
+    await message.reply(response)
 
 
 # обробник події приєднання нових користувачів до групи
@@ -43,10 +74,5 @@ async def greet_new_members(message: types.Message):
         # відправка привітання новому користувачу
         await message.answer(f"Вітаємо, {user.full_name}! Ласкаво просимо в наш родинний чат.")
 
-# запуск бота
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
-
-
-
